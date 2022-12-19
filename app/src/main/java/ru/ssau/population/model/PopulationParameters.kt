c2ce33c1ba7da073d198c2341888f5b2efcb505d
Fomin <vyacheslav.fomin@magenta-technology.com>
2022-12-19 11:25:03 +0400

    code cleaned up

--- app/src/main/java/ru/ssau/population/model/PopulationParameters.kt
+++
@@ -1,9 +0,0 @@
-package ru.ssau.ais.model
-
-data class PopulationParameters(
-    override val selfReproductionFactor: Double,
-    override val attackFactor: Double,
-    override val defenseFactor: Double,
-    override val nutrition: Double,
-    override val hungerFactor: Double,
-) : PopulationStats
--- app/src/main/java/ru/ssau/population/model/PopulationState.kt
+++
@@ -1,7 +0,0 @@
-package ru.ssau.ais.model
-
-// одна популяция в конкретный момент времени
-interface PopulationState {
-    val population: PopulationStats
-    val count: Long
-}
